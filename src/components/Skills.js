37c4dfbb0e38ee559bb0a63c48daafb3d87a8830
BrayOA <borconarellano@gmail.com>
2023-06-20 22:09:44 -0500

    Projects

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -39,19 +39,19 @@ export const Skills = () => {
                         <Carousel responsive={responsive} infinite={true} className="owl-carousel owl-theme skill-slider">
                             <div className="item">
                                 <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <h5>Web Development Frontend</h5>
                             </div>
                             <div className="item">
                                 <img src={meter2} alt="Image" />
-                                <h5>Brand Identity</h5>
+                                <h5>HTMl</h5>
                             </div>
                             <div className="item">
                                 <img src={meter3} alt="Image" />
-                                <h5>Logo Design</h5>
+                                <h5>CSS-Grid-StyleComponents</h5>
                             </div>
                             <div className="item">
                                 <img src={meter1} alt="Image" />
-                                <h5>Web Development</h5>
+                                <h5>JavaScript-React</h5>
                             </div>
                         </Carousel>
                     </div>
